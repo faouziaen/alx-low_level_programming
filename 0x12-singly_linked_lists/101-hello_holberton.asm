8a0759c93ee26d298c011134f2c248d85f8013af
faouziaen <faouziaennaama@gmail.com>
2023-08-24 17:54:14 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,9 @@
-section .data
-    hello db "Hello, Holberton", 0
-    format db "%s", 10, 0
-
-section .text
-    global main
-
+global   main
+	  extern    printf
 main:
-    ; Call printf with the message and format string
-    mov rdi, format
-    mov rsi, hello
-    xor rax, rax ; Clear RAX for printf return value
-    call printf
-
-    ; Exit the program
-    mov rax, 60 ; syscall number for exit
-    xor rdi, rdi ; status code 0
-    syscall
-
-section .bss
-    resb 8 ; Reserve space for printf return value
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
