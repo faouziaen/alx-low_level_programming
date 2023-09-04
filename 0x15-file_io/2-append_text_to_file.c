29a5fd051ffdcd70f9037db6a9bb2e0efaa0eac0
faouziaen <faouziaennaama@gmail.com>
2023-09-04 16:06:01 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,20 +15,18 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-		return (-1);
-	if (text_content)
+	if (text_content != NULL)
 	{
-		while (text_content[len])
+		for (len = 0; text_content[len];)
 			len++;
-		byt_wr = write(fd, text_content, len);
-		if (byt_wr == -1)
-		{
-			close(fd);
-			return (-1);
-		}
 	}
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	byt_wr = write(fd, text_content, len);
+
+	if (fd == -1 || byt_wr == -1)
+		return (-1);
+
 	close(fd);
 	return (1);
 }
