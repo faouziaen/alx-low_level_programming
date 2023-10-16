69aed5ba53f25075833c163a6e288a99ffa0c52e
faouziaen <faouziaennaama@gmail.com>
2023-10-16 14:19:33 +0100

    100-operations.

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,27 +1,71 @@
-// 100-operations.c
+#include <stdio.h>
 
-int add(int a, int b) {
-    return a + b;
+/**
+ * add - Add two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The sum of the integers.
+ */
+int add(int a, int b)
+{
+	return (a + b);
 }
 
-int sub(int a, int b) {
-    return a - b;
+/**
+ * sub - Subtract two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The result of the subtraction (a - b).
+ */
+int sub(int a, int b)
+{
+	return (a - b);
 }
 
-int mul(int a, int b) {
-    return a * b;
+/**
+ * mul - Multiply two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The product of the integers.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
 }
 
-int divide(int a, int b) {
-    if (b == 0) {
-        return -1; // Handle division by zero
-    }
-    return a / b;
+/**
+ * divide - Divide two integers.
+ * @a: First integer (numerator).
+ * @b: Second integer (denominator).
+ *
+ * Return: The result of the division (a / b).
+ */
+int divide(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error: Division by zero\n");
+		return (-1);
+	}
+	return (a / b);
 }
 
-int mod(int a, int b) {
-    if (b == 0) {
-        return -1; // Handle modulo by zero
-    }
-    return a % b;
+/**
+ * mod - Compute the remainder of the division of two integers.
+ * @a: First integer (dividend).
+ * @b: Second integer (divisor).
+ *
+ * Return: The remainder of the division (a % b).
+ */
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error: Division by zero\n");
+		return (-1);
+	}
+	return (a % b);
 }
