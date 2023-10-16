45192bd41c0bac5f4a0eb7d7c7e5b758a278db89
faouziaen <faouziaennaama@gmail.com>
2023-10-16 12:22:17 +0100

    libdynamic.so

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower- Write a function that checks for lowercase character
+ *Return: Returns: 1 if c is lowercase
+ *@c: is the char to be checked
+ * Returns: 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
